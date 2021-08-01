526a2045953984ef7a31b55e295c894e86d8fc64
Alastair Thomson <alastair0194@gmail.com>
2021-08-01 12:24:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# US Medical Insurance analysis 
+ Analysed US Medical insurance according to sex, region, number of children and msoking status
---
+++ analysis.py
@@ -0,0 +1,7 @@
+import csv
+with open('insurance.csv', 'r') as insurance_csv:
+	file_read = csv.read()
+	print(file_read)
+
+
+print("Hello World!")
